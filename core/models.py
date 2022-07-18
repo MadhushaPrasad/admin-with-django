402b3f8e4b0a8babb5a945580d40f190f30d613c
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-18 19:08:27 +0530

    User model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255,uniq=true)
+    password = models.CharField(max_length=255,uniq=true)
+    is_ambassador = models.BooleanField(default=true)
+    username = None
+
+    USERNAME_FIELD = 'email'
